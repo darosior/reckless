91a170ac853e9ba5c4e52b6339227c0a97b58425
darosior <darosior@protonmail.com>
2019-07-07 03:40:13 +0200

    Better user output on 'install' command, cleaning

--- bug.py
+++ bug.py
@@ -42,25 +42,28 @@ def install(plugin, url, main_file=None, install_dir=None):
     :install_dir: (optional) The name of the directory to create in c-lightning's
                 default plugins directory.
     """
-    # We use the command return to "log" for the user
-    response = ""
+    # We use the command return to give an output to the user
+    response = ["===== Installation result ======"]
     res_name = urllib.parse.urlparse(url).path.split('/')[-1]
     if not install_dir:
         install_dir = res_name.split('.')[0]
     install_path = os.path.join(plugin.plugins_path, install_dir)
-    response += "Creating {} directory ..".format(install_path)
     create_dir(install_path)
+    response.append("Created {} directory ..".format(install_path))
     file_path = os.path.join(install_path, res_name)
     if os.path.exists(file_path):
         return "Destination {} already exists".format(file_path)
-    response += "Downloading file from {} to {}\n".format(url, file_path)
+    # A special case to handle repositories with many plugins as folders
     if "api.github.com" in url and len(res_name.split('.')) == 1:
         dl_folder_from_github(install_path, url)
     else:
         urllib.request.urlretrieve(url, file_path)
+    response.append("Downloaded file from {} to {}".format(url, file_path))
+    # If the file has been urlretrieved, it's wether an archive or a single
+    # file
     if file_path.endswith(".tar.gz") or file_path.endswith("tar") \
             or file_path.endswith(".zip"):
-        response += "Extracting the archive {}\n".format(res_name)
+        response.append("Extracting the archive {}".format(res_name))
         if file_path.endswith(".tar.gz") or file_path.endswith("tar"):
             with tarfile.open(file_path, "r:*") as tar_file:
                 tar_file.extractall(install_path)
@@ -69,30 +72,28 @@ def install(plugin, url, main_file=None, install_dir=None):
             with zipfile.ZipFile(file_path, "r") as zip_file:
                 zip_file.extractall(install_path)
             os.remove(file_path)
-    response += "Searching for a main file in {} to make executable\n"\
-                .format(res_name)
-    # Trying to figure out which file to set executable
+    # Trying to figure out which file to set executable, otherwise we would not
+    # be able to load the plugin
     possible_filenames = {file_path.split('/')[-1].split('.')[0], "main",
                           "plugin"}
     main_file = get_main_file(possible_filenames, install_path)
-    # The above function can return None
+    # We might not have found the main_file...
     if main_file:
-        response += "Making {} executable\n".format(main_file)
-        try:
-            make_executable(os.path.join(install_path, main_file))
-        except:
-            return "Could not set {} executable".format(main_file)
+        response.append("Making {} executable".format(main_file))
+        make_executable(os.path.join(install_path, main_file))
     else:
-        response += "Could not find a main file, hence not making anything\
-                    executable\n"
-    response += "Reloading plugins from lightningd\n"
+        response.append("Could not find a main file, hence not making anything\
+                        executable")
     plugin.rpc.plugin(command="reload")
-    response += "Waiting for a second to check if plugin has been loaded\n"
+    response.append("Reloaded plugins from lightningd")
+    response.append("Waiting for a second to check if the brand new plugin\
+                     has been loaded")
     time.sleep(1)
     active_plugins = plugin.rpc.plugin(command="list")
-    response += "Active plugins : "\
-                + ", ".join(p.split('/')[-1] for p in active_plugins) + '\n'
+    response.append("Active plugins : "
+                    + ", ".join(p.split('/')[-1] for p in active_plugins))
     return response
+
 
 @plugin.method("search", desc=search_description, long_desc=search_long_description)
 def search(plugin, keyword):
@@ -104,7 +105,9 @@ def search(plugin, keyword):
     github_repos = {"lightningd/plugins", "conscott/c-lightning-plugins",
                     "renepickhardt/c-lightning-plugin-collection"}
     github_urls = search_github(github_repos, keyword)
-    urls = github_urls # + another source + another source + ...
+    # Hopefully more sources to come !
+    urls = github_urls
     return urls if urls else "Could not find any plugin matching this keyword"
+
 
 plugin.run()
